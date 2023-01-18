2073a5baf20c36af57255be9cd3ce3a8659ab937
smbatshahbazyan <smbatshahbazyan@mail.com>
2023-01-18 13:40:24 +0400

    Add Binary to Decimal homework

--- src/BinaryToDecimal.java
+++ src/BinaryToDecimal.java
@@ -7,7 +7,7 @@ public class BinaryToDecimal {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i]);
         }
-        for (int i = arr.length - 1; i >= 0; i--, div *=2) {
+        for (int i = arr.length - 1; i >= 0; i--, div *= 2) {
             if (arr[i] == 1) {
                 decimal += div;
             }
