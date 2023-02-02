359f8dd5bf305ea41e2ed08b68206449e1f1fc52
smbatshahbazyan <smbatshahbazyan@mail.com>
2023-02-02 13:58:17 +0400

    Change method first letter to lowercase

--- src/HomeworkFromMethods/CheckArmstrong.java
+++ src/HomeworkFromMethods/CheckArmstrong.java
@@ -7,31 +7,28 @@ public class CheckArmstrong {
         Scanner s = new Scanner(System.in);
         System.out.println("Input number to check is this number armstrong or not? ");
         int number = s.nextInt();
-        if (IsArmstrong(number)) {
+        if (isArmstrong(number)) {
             System.out.println("This number is armstrong");
         } else {
             System.out.println("This number isn't armstrong");
         }
     }
 
-    public static boolean IsArmstrong(int number) {
-        if (number > 1 && number < 10) {
-            return false;
-        }
-        return number == Armstrong(number);
+    public static boolean isArmstrong(int number) {
+        return number == armstrong(number);
     }
 
-    public static int Armstrong(int num) {
+    public static int armstrong(int num) {
         int counter = 0;
-        int powerNumber = NumberOfDigits(num);
+        int powerNumber = numberOfDigits(num);
         for (int i = 0; i < powerNumber; i++) {
-            counter += Power(num % 10, powerNumber);
+            counter += power(num % 10, powerNumber);
             num /= 10;
         }
         return counter;
     }
 
-    public static int Power(int a, int b) {
+    public static int power(int a, int b) {
         int counter = 1;
         for (int i = 1; i <= b; i++) {
             counter *= a;
@@ -39,7 +36,7 @@ public class CheckArmstrong {
         return counter;
     }
 
-    public static int NumberOfDigits(int a) {
+    public static int numberOfDigits(int a) {
         int counter = 0;
         for (int i = a; i >= 1; i /= 10) {
             counter++;
--- src/HomeworkFromMethods/MethodPowerOfNumber.java
+++ src/HomeworkFromMethods/MethodPowerOfNumber.java
@@ -7,12 +7,12 @@ public class MethodPowerOfNumber {
         Scanner s = new Scanner(System.in);
         System.out.println("Please input two integer from -10 to 10 for counting first number power of second number ");
         System.out.println("First number must be positive, For example");
-        System.out.println("2^5=" + PowerOfNumber(2, 5));
-        System.out.println("5^-3=" + PowerOfNumber(5, -3));
+        System.out.println("2^5=" + powerOfNumber(2, 5));
+        System.out.println("5^-3=" + powerOfNumber(5, -3));
         System.out.println("Please input two numbers");
         int number1 = s.nextInt();
         int number2 = s.nextInt();
-        double powerResult = PowerOfNumber(number1, number2);
+        double powerResult = powerOfNumber(number1, number2);
         if (powerResult == -1) {
             System.out.println("Please input correct numbers, fro-10 to 10");
         } else if (powerResult == -2) {
@@ -22,7 +22,7 @@ public class MethodPowerOfNumber {
         }
     }
 
-    public static double PowerOfNumber(int num1, int num2) {
+    public static double powerOfNumber(int num1, int num2) {
         double counter = 1;
         if (num1 <= 0) {
             counter = -2;
@@ -31,14 +31,14 @@ public class MethodPowerOfNumber {
             counter = -1;
             return counter;
         } else {
-            for (int i = 1; i <= CheckModule(num2); i++) {
+            for (int i = 1; i <= checkModule(num2); i++) {
                 counter *= num1;
             }
         }
         return num2 < 0 ? 1 / counter : counter;
     }
 
-    public static int CheckModule(int number) {
+    public static int checkModule(int number) {
         if (number < 0) {
             number *= -1;
         }
