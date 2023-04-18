ba6c78a94b6e186075e94bdea30573a29c3a4db6
smbatshahbazyan <smbatshahbazyan@mail.com>
2023-04-19 03:53:43 +0400

    Add 3 missed out homeworks

--- src/HomeworkSinceApril/HomeworkSchool/Main.java
+++ src/HomeworkSinceApril/HomeworkSchool/Main.java
@@ -2,13 +2,11 @@ package HomeworkSinceApril.HomeworkSchool;
 
 public class Main {
     public static void main(String[] args) {
-        HeadTeacher hd1 = new HeadTeacher("Aram", "Aramyan", 150000, 1.5);
-        System.out.println(hd1.getSalary());
-        hd1.setCoefficient(2.5);
+        Teacher hd1 = new HeadTeacher("Aram", "Aramyan", 150000, 1.5);
         System.out.println(hd1.getSalary());
         Teacher t1 = new Teacher("Petros", "Petrosyan", 150000);
         System.out.println(t1.getSalary());
-        SchoolDirector sd1 = new SchoolDirector("Poghos", "Poghosyan", 300000);
+        Teacher sd1 = new SchoolDirector("Poghos", "Poghosyan", 300000);
         System.out.println(sd1.getSalary());
     }
 }
